0ee0a260e740256bd1da9facf95ca29d7905c3f4
Vilmen <abramian.vl@phystech.edu>
2024-10-21 17:01:37 +0300

    Bug fix with avatar

--- backend/users/views.py
+++ backend/users/views.py
@@ -46,15 +46,17 @@ class UserViewSet(viewsets.ModelViewSet):
             permission_classes=[IsAuthenticated])
     def avatar(self, request, *args, **kwargs):
         '''Добавить или изменить аватар пользователя'''
-        new_avatar = request.data.get('avatar')
-        if new_avatar is None:
-            if request.method == 'DELETE':
-                return Response('Изображение профиля успешно удалено', status=status.HTTP_204_NO_CONTENT)
-            return Response(status=status.HTTP_400_BAD_REQUEST)
         user = self.request.user
-        serializer = UserSerializer(user, data={'avatar': new_avatar})
-        if serializer.is_valid():
-            serializer.save()
-        if request.method == 'PUT':
-            return Response(serializer.data)
-        
+        if request.method == 'DELETE':
+            return Response('Изображение профиля успешно удалено', status=status.HTTP_204_NO_CONTENT)
+        serializer = UserSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        new_avatar = serializer.validated_data.get('avatar')
+        user.avatar = new_avatar
+        user.save()
+        if new_avatar is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        image_url = request.build_absolute_uri(
+            request.user.avatar.url
+        )
+        return Response({'avatar': str(image_url)}, status=status.HTTP_200_OK)
