dae75821a4c2d3ecb52c3bfcdbbcc8f9fa75c6ec
Vilmen <abramian.vl@phystech.edu>
2024-11-08 17:07:36 +0300

    Fix representation of recipe and add pagination

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,20 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from .serializers import IngredientSerializer, RecipeReadSerializer, RecipeWriteSerializer, TagSerializer
+from .paginations import ApiPagination
+from .serializers import (
+    IngredientSerializer,
+    RecipeReadSerializer,
+    RecipeWriteSerializer,
+    TagSerializer
+)
 from recipes.models import Ingredient, Recipe, Tag
+
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
@@ -19,6 +27,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
+    pagination_class = ApiPagination
 
     def get_serializer_class(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
