e5c79ce019c1f986fb3000f4b1abc6669b080c87
Vilmen <abramian.vl@phystech.edu>
2024-11-21 14:19:42 +0300

    Complete recipes tests

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from .filters import RecipeFilter
@@ -29,6 +31,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     pagination_class = ApiPagination
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
 
@@ -38,6 +41,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return RecipeReadSerializer
 
     def partial_update(self, request, *args, **kwargs):
+        obj = get_object_or_404(Recipe, pk=kwargs['pk'])
+
         if 'ingredients' not in request.data:
             return Response(
                 {'ingredients': 'Это обязательное поле!'},
@@ -48,4 +53,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 {'tags': 'Это обязательное поле!'},
                 status=status.HTTP_400_BAD_REQUEST
             )
+        if request.user != Recipe.objects.get(id=kwargs['pk']).author:
+            return Response(
+                {'users': 'Нельзя редактировать чужую запись!'},
+                status=status.HTTP_403_FORBIDDEN
+            )
         return super().partial_update(request, *args, **kwargs)
+    
+    @action(detail=True,
+            methods=['get'],
+            url_path='get-link'
+        )
+    def get_link(self, *args, **kwargs):
+        return Response(
+            {'short-link': f'https://example.com/recipes/{kwargs}'}
+        )
