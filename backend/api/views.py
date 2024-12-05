0611ae96344672bef3fcc1a15d9e1c2dcea2cca9
Vilmen <abramian.vl@phystech.edu>
2024-12-05 15:55:41 +0300

    Merge api in one app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
-
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from djoser.serializers import SetPasswordSerializer
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -15,13 +16,15 @@ from .serializers import (
     RecipeMiniSerializer,
     RecipeReadSerializer,
     RecipeWriteSerializer,
+    SubscriptionsSerializer,
     TagSerializer
 )
 from recipes.models import (
     Ingredient, FavoriteRecipes,
     Recipe, ShoppingList, Tag
 )
-from recipes.models import User
+from recipes.models import Subscriptions, User
+from recipes.serializers import CreateUserSerializer, UserSerializer
 
 
 class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
@@ -179,3 +182,127 @@ def shopping_cart(request, author):
     )
 
     return text_version
+
+
+class MyPagePagination(PageNumberPagination):
+    page_size_query_param = 'limit'
+    page_size = 6
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    pagination_class = MyPagePagination
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreateUserSerializer
+        return UserSerializer
+
+    @action(detail=False,
+            methods=['get', 'post'],
+            permission_classes=[IsAuthenticated])
+    def me(self, request):
+        '''Получить свой профиль'''
+        user = self.request.user
+        serializer = UserSerializer(user, context={'request': request})
+        return Response(serializer.data)
+
+    @action(detail=False,
+            methods=['post'],
+            permission_classes=[IsAuthenticated])
+    def set_password(self, request, *args, **kwargs):
+        '''Изменить пароль'''
+        serializer = SetPasswordSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+        if serializer.is_valid(raise_exception=True):
+            self.request.user.set_password(serializer.data['new_password'])
+            self.request.user.save()
+            return Response(
+                'Пароль успешно изменён',
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True,
+            methods=['post', 'delete'],
+            permission_classes=[IsAuthenticated])
+    def subscribe(self, request, *args, **kwargs):
+        '''Создать и/или удалить подписку'''
+        author = get_object_or_404(User, id=self.kwargs.get('pk'))
+        recipes_limit = request.query_params.get('recipes_limit', None)
+        if request.method == 'POST':
+            if recipes_limit is not None:
+                recipes_limit = int(recipes_limit)
+            if self.request.user == author:
+                return Response(
+                    'Подписка на себя невозможна!',
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if Subscriptions.objects.filter(
+                user=self.request.user, author=author
+            ).exists():
+                return Response(
+                    {'detail': 'Вы уже подписаны на этого пользователя!'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            Subscriptions.objects.create(user=self.request.user, author=author)
+            serializer = SubscriptionsSerializer(
+                author, context={'request': request, 'limit': recipes_limit}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == 'DELETE':
+            if not Subscriptions.objects.filter(
+                user=self.request.user, author=author
+            ).exists():
+                return Response(
+                    'Нельзя отписаться от несуществующей подписки!',
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            Subscriptions.objects.get(author=author).delete()
+            return Response(
+                'Успешная отписка',
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+    @action(detail=False)
+    def subscriptions(self, request):
+        '''Отобразить все подписки пользователя'''
+        recipes_limit = request.query_params.get('recipes_limit', None)
+        if recipes_limit is not None:
+            recipes_limit = int(recipes_limit)
+
+        subscriptions = Subscriptions.objects.filter(user=self.request.user)
+        user_subscriptions = [
+            subscription.author for subscription in subscriptions
+        ]
+        pages = self.paginate_queryset(user_subscriptions)
+        serializer = SubscriptionsSerializer(
+            pages, many=True,
+            context={'request': request, 'limit': recipes_limit}
+        )
+        return self.get_paginated_response(serializer.data)
+
+    @action(detail=True,
+            methods=['get', 'post', 'put', 'delete'],
+            permission_classes=[IsAuthenticated])
+    def avatar(self, request, *args, **kwargs):
+        '''Добавить или изменить аватар пользователя'''
+        user = self.request.user
+        if request.method == 'DELETE':
+            return Response(
+                'Изображение профиля успешно удалено',
+                status=status.HTTP_204_NO_CONTENT
+            )
+        serializer = UserSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        new_avatar = serializer.validated_data.get('avatar')
+        user.avatar = new_avatar
+        user.save()
+        if new_avatar is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        image_url = request.build_absolute_uri(
+            request.user.avatar.url
+        )
+        return Response({'avatar': str(image_url)}, status=status.HTTP_200_OK)
