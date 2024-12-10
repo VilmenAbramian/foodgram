40ba76636d1a855d1faaaae2822a99c240f9a635
Vilmen <abramian.vl@phystech.edu>
2024-12-10 16:14:33 +0300

    Add import fixtures commands

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,23 @@
+import json
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+class Command(BaseCommand):
+    help = 'Импортирует ингредиенты из data/ingredients.json'
+
+    def handle(self, *args, **kwargs):
+        try:
+            with open('data/ingredients.json', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            ingredients = [
+                Ingredient(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                ) for item in data
+            ]
+            Ingredient.objects.bulk_create(ingredients, ignore_conflicts=True)
+
+            self.stdout.write(self.style.SUCCESS('Ингредиенты успешно импортированы.'))
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f'Ошибка: {e}'))
