62998521d2e44bad34cceeee562ec2b2814c892c
Vilmen <abramian.vl@phystech.edu>
2024-12-16 13:51:08 +0300

    Fix api

--- backend/api/shopping_cart.py
+++ backend/api/shopping_cart.py
@@ -1,20 +1,21 @@
 from datetime import date
 
 
-def shopping_cart(filling_basket):
-    HEADER_TEMPLATE = "Список покупок от {date}"
-    PRODUCT_TEMPLATE = "{index}) {name} - {amount} {unit}"
-    RECIPE_TEMPLATE = "{index}) {name}"
+HEADER_TEMPLATE = "Список покупок от {date}"
+PRODUCT_TEMPLATE = "{index}) {name} - {amount} {unit}"
+RECIPE_TEMPLATE = "{index}) {name}"
 
+
+def shopping_cart(filling_basket):
     header = HEADER_TEMPLATE.format(date=date.today().strftime('%d.%m.%Y'))
     products = [
         PRODUCT_TEMPLATE.format(
-            index=i,
+            index=index,
             name=ingredient["ingredient__name"].capitalize(),
             amount=ingredient["total_amount"],
             unit=ingredient["ingredient__measurement_unit"]
         )
-        for i, ingredient in enumerate(filling_basket, start=1)
+        for index, ingredient in enumerate(filling_basket, start=1)
     ]
     recipes = [
         RECIPE_TEMPLATE.format(index=i, name=recipe)
