5ee6423dd1eb3d54fc1f46c44bcd44184352aa85
Vilmen <abramian.vl@phystech.edu>
2024-12-18 14:48:31 +0300

    Fix flake8

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from django.db.models import Sum
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404, redirect
 from django_filters.rest_framework import DjangoFilterBackend
-from django.views import View
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
